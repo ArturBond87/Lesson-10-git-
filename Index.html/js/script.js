6f304098df5ac6800d1ba56ab355271d1a0cec42
Artur <exemple@gmail.com>
2022-03-01 11:14:23 +0200

    first commit

---
+++ Index.html/js/script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+// let number = 4.6;
+
+// console.log(-4/0);
+// console.log('string' * 9);
+
+// const person = "Alex";
+// const bool = true;
+
+// console.log(something); //тип данных null
+
+// let und;
+// console.log(und); //тип данных undefined
+
+const obj = { //тип данных обьект
+    name: "Jon",
+    age: 25,
+    isMarried: false
+};
+
+// console.log(obj.name);
+console.log(obj["name"]);
+
+let arr = ['plum.png','orange.jpg','apple.bmp'];//тип данных массив
+console.log(arr[1]);
---
+++ Lesson Nr.7/js/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// alert('Hello');
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = prompt("Вам есть 18?", "18");
+// console.log(typeof(answer));
+
+const answers = [];
+
+// answers[0] = prompt('Your name?', '');
+// answers[1] = prompt('How old are you?', '');
+// answers[2] = prompt('Your surename?', '');
+
+console.log(typeof(answers));
